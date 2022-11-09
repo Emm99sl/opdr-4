3bc1330fa1c3db454ecabc56c71f656ddf1fb59d
Emm99sl <114397990+Emm99sl@users.noreply.github.com>
2022-11-09 11:22:21 +0100

    Update Dashboard_Eind_streamlit.py

--- Dashboard_Eind_streamlit.py
+++ Dashboard_Eind_streamlit.py
@@ -459,11 +459,11 @@ def add_categorical_legend(folium_map, title, colors, labels):
         }
     </style>
     """
-st.subheader('Kaart van de winst ($) per superstore')
+
     folium_map.get_root().header.add_child(folium.Element(script + css))
 
     return folium_map
-
+st.subheader('Kaart van de winst ($) per superstore')
 m2 = folium.Map(location = [37.09024, -95.712891], zoom_start = 4.4)
 
     
