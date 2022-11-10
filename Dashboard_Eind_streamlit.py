353191d8760aad109e03d48c10805d19b639e8c2
Emm99sl <114397990+Emm99sl@users.noreply.github.com>
2022-11-10 13:30:57 +0100

    Update Dashboard_Eind_streamlit.py

--- Dashboard_Eind_streamlit.py
+++ Dashboard_Eind_streamlit.py
@@ -186,6 +186,7 @@ with tab1:
        fig3.add_trace(go.Histogram(x = off_supp['Kosten'], name = 'Office supply'))
        slider = [
               {'steps':[
+                     {'method': 'update', 'label':'Kosten histogram per categorie', 'args':[{'visible': [True, True, True]}]},
                      {'method': 'update', 'label':'Kosten technologie', 'args':[{'visible': [True, False, False]}]},
                      {'method': 'update', 'label':'Kosten meubilair', 'args':[{'visible': [False, True, False]}]},
                      {'method': 'update', 'label':'Kosten kantoor artikelen', 'args':[{'visible': [False, False, True]}]}]}]
